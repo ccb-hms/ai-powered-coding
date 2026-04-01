553cac6194b731887eec2ec07029683c1c7cdb30
Anthony Christidis <anthony.christidis@stat.ubc.ca>
2026-04-01 19:30:39 -0400

    Refactor function name to improve readability and consistency in utils.R

--- live-demo/utils.R
+++ live-demo/utils.R
@@ -4,11 +4,11 @@
 
 # Automatically handles NA values by removing them before calculation.
 
-calculateLog2Fc <- function(treatment_vals, control_vals) {
+calculate_log2_fc <- function(treatment_vals, control_vals) {
   t_mean <- mean(treatment_vals, na.rm = TRUE)
   c_mean <- mean(control_vals, na.rm = TRUE)
-  
-  if(is.na(c_mean) || c_mean == 0) return(NA)
-  
-  return(log2(t_mean / c_mean))
+
+  if (is.na(c_mean) || c_mean == 0) return(NA)
+
+  log2(t_mean / c_mean)
 }
